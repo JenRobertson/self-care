fc554d14aaafc9f6d28735fcb0020534be8af2d6
JenRobertson <jenrobertsonjen@gmail.com>
2021-08-07 21:19:31 +0100

    add font and things

--- house/code.js
+++ house/code.js
@@ -1,6 +1,14 @@
 const container = document.getElementById('container');
 const player = document.getElementById('player');
 
+const data = {
+    tasksCompleted: 0,
+    player: {
+        x: 0,
+        y: 0
+    }
+};
+
 container.onclick = function(e){
   walkTo(e.clientX, e.clientY);
   hideAllButtons();
@@ -14,6 +22,12 @@ sink.onclick = function(e){
 const walkTo = (x, y) => {
   player.style.left = `${x}px`;
   player.style.top = `${y}px`;
+
+  if(x < data.player.x) player.style.transform = 'scaleX(1)';
+  if(x > data.player.x) player.style.transform = 'scaleX(-1)';
+
+  data.player.x = x;
+  data.player.y = y;
 }
 
 const hideAllButtons = () => {
