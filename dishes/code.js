dc4288190fa22d92afe1da4fb4764eaf64547978
eleanombre <eleanor.keane@bbc.co.uk>
2021-08-08 16:32:44 +0100

    Dish cleaning

--- dishes/code.js
+++ dishes/code.js
@@ -1,39 +1,27 @@
 const paw = document.getElementById('cleaning_paw');
 const dish = document.getElementById('dish_1');
+const stains = document.getElementById('dish_1_stain');
 
 const zoomOfImages = 4; 
 const offsetX = 10;
 const offsetY = 20;
 
-console.dir(dish);
+let movesInDishSpace = 0;
+const isInDishSpace = (x) => {
+  return (x >= dish.x && x <= dish.x + dish.offsetWidth);
+}
 
+const dishSpaceMove = () => {
+  movesInDishSpace += 1;
+  if (movesInDishSpace >= 150) {
+    stains.style.display = "none";
+    window.parent.postMessage('done', '*');
+  }
+}
 paw.addEventListener('mousemove', (e) => {
   if (e.clientX == 0) return;
+  if (isInDishSpace(paw.x)) {
+    dishSpaceMove();
+  }
   paw.style.left = `${e.clientX / zoomOfImages - offsetX}px`;
-})
-
-// socks.forEach((sock) => {
-
-//   sock.addEventListener('drag', (e) => {
-//     if(e.clientX == 0) return;
-//     sock.style.left = `${e.clientX / zoomOfImages - offsetX}px`;
-//     sock.style.top = `${e.clientY/ zoomOfImages - offsetY}px`;
-
-//   })
-//   sock.addEventListener('dragend', (e) => {
-//     sock.style.left = `${e.clientX / zoomOfImages - offsetX}px`;
-//     sock.style.top = `${e.clientY/ zoomOfImages - offsetY}px`;
-
-//   })
-// });
-
-// document.addEventListener("drop", function( event ) {
-//     // prevent default action (open as link for some elements)
-//     event.preventDefault();
-//     // move dragged elem to the selected drop target
-//     console.log('poo')
-//     if ( event.target.id == "basket-front" ) {
-//         console.log('in')
-//     }
-  
-// }, false);
+});
