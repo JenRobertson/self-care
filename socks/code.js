69f05fb4738640403a687b1b2e012fba0fcf4114
JenRobertson <jenrobertsonjen@gmail.com>
2021-08-08 15:30:53 +0100

    socks game dropping

--- socks/code.js
+++ socks/code.js
@@ -3,29 +3,33 @@ const socks = document.querySelectorAll('.sock');
 const zoomOfImages = 4; 
 const offsetX = 10;
 const offsetY = 20;
+let socksInBasket = 0;
 
 socks.forEach((sock) => {
-
+  
   sock.addEventListener('drag', (e) => {
     if(e.clientX == 0) return;
     sock.style.left = `${e.clientX / zoomOfImages - offsetX}px`;
     sock.style.top = `${e.clientY/ zoomOfImages - offsetY}px`;
-
+    
   })
   sock.addEventListener('dragend', (e) => {
     sock.style.left = `${e.clientX / zoomOfImages - offsetX}px`;
     sock.style.top = `${e.clientY/ zoomOfImages - offsetY}px`;
-
+    
   })
 });
 
-// document.addEventListener("drop", function( event ) {
-//     // prevent default action (open as link for some elements)
-//     event.preventDefault();
-//     // move dragged elem to the selected drop target
-//     console.log('poo')
-//     if ( event.target.id == "basket-front" ) {
-//         console.log('in')
-//     }
+document.addEventListener("dragover", function(event) {
+  event.preventDefault();
+});
+
+basketArea.addEventListener("drop", function( event ) {
+  event.preventDefault();
+  console.log(event.target.id);
   
-// }, false);
+  if ( event.target.id == "basket-front" ) {
+    socksInBasket++;
+    progressInner.style.width = (socksInBasket / socks.length) * 100 + '%';
+  }
+}, false);
